9ecfe0dc202bddaacf24f0f6a52313a1825b8f07
Victor Williams <victor.williams1030@gmail.com>
2020-05-29 16:23:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,10 +45,9 @@ class Song
   def self.new_from_filename 
     name_array = name.sub!(/.mp3/,"").split(" - ") # needed a lil' regex razzle dazzle
     @song = self.new 
-    song.artist_name = name_array[0] 
+    @song.artist_name = name_array[0] 
     # the artist's name is the first element in the array, so index = 0 
-    song.name = name_array[1]
-    
-    
+    @song.name = name_array[1]
+    @song
   end
 end
